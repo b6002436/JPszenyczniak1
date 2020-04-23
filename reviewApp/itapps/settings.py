1eceef5ecad87602d3548aa4019138d22cb559ab
student <student@b6002436.localdomain>
2020-04-23 17:01:48 +0100

    Users can reset Password

--- reviewApp/itapps/settings.py
+++ reviewApp/itapps/settings.py
@@ -131,3 +131,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL= 'itreporting-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS  = True
+EMAIL_HOST_USER = 'sysarchtest@gmail.com'
+EMAIL_HOST_PASSWORD = 'sysarchstest123'
